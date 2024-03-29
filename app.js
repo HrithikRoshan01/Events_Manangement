de66e076babb59ba512c1514ba96483693cca852
Hrithik Roshan <163551846+HrithikRoshan01@users.noreply.github.com>
2024-03-29 14:31:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,21 +11,16 @@ function parseCSV(filePath) {
     fs.createReadStream(filePath)
       .pipe(csv())
       .on('data', (row) => {
-        // Process each row
         data.push(row);
       })
       .on('end', () => {
-        // CSV parsing is complete
         console.log('CSV file parsed successfully.');
-        // Resolve the promise with the parsed data
         resolve(data);
       })
       .on('error', (error) => {
-        // Reject the promise with the error
         reject(error);
       });
   });
 }
 
-// Export the function to parse CSV
 module.exports = parseCSV;
